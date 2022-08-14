6317af819ae2e37ac6ebf1802a5e6459e71426ff
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-08-14 21:05:51 +0530

    Meta tags added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,11 +3,48 @@
 	import Nav from '../components/nav.svelte';
 	import { onMount } from 'svelte'
   	import { themeChange } from 'theme-change'
+	import { MetaTags, JsonLd } from 'svelte-meta-tags';
 
 	onMount(() => {
     	themeChange(false)
   	})
 </script>
+
+<MetaTags
+  title="Stockify"
+  titleTemplate = '%s | Learn how to invest'
+  description="This example uses more of the available config options."
+  openGraph={{
+    url: 'https://stockify.prathamprasoon.com',
+    title: 'Stockify',
+    description: "Learn how to trade and invest with stockify's real life simulator, with no risk.",
+    images: [
+      {
+        url: 'cover.png',
+        width: 800,
+        height: 600,
+        alt: 'Stockify cover image'
+      },
+      {
+        url: 'cover.png',
+        width: 900,
+        height: 800,
+        alt: 'Stockify cover image'
+      },
+      { url: 'cover.png' },
+      { url: 'cover.png' }
+    ],
+    site_name: 'Stockify'
+  }}
+  twitter={{
+    handle: '@Stockify_',
+    cardType: 'summary_large_image',
+    title: 'Using More of Config',
+    description: "Learn how to trade and invest with stockify's real life simulator, with no risk.",
+    image: 'cover.png',
+    imageAlt: 'Stockify cover image'
+  }}
+/>
 
 <Nav />
 <slot />
