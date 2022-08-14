751908239e824d0d00b79d67f7a59723037acc59
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-08-14 23:54:37 +0530

    fixes (?)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,48 +2,53 @@
 	import '../app.css';
 	import Nav from '../lib/nav.svelte';
 	import { onMount } from 'svelte';
-  	import { themeChange } from 'theme-change';
+	import { themeChange } from 'theme-change';
 	import { MetaTags } from 'svelte-meta-tags';
 
 	onMount(() => {
-    	themeChange(false)
-  	})
+		themeChange(false);
+	});
 </script>
 
 <MetaTags
-  title="Stockify"
-  titleTemplate = '%s | Learn how to invest'
-  description="Learn how to trade and invest with stockify's real life simulator, at no risk."
-  openGraph={{
-    url: 'https://stockify.prathamprasoon.com',
-    title: 'Stockify',
-    description: "Learn how to trade and invest with stockify's real life simulator, at no risk.",
-    images: [
-      {
-        url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png',
-        width: 800,
-        height: 600,
-        alt: 'Stockify cover image'
-      },
-      {
-        url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png',
-        width: 900,
-        height: 800,
-        alt: 'Stockify cover image'
-      },
-      { url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png' },
-      { url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png' }
-    ],
-    site_name: 'Stockify'
-  }}
-  twitter={{
-    handle: '@Stockify_',
-    cardType: 'summary_large_image',
-    title: 'Stockify | Learn how to invest',
-    description: "Learn how to trade and invest with stockify's real life simulator, at no risk.",
-    image: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png',
-    imageAlt: 'Stockify cover image'
-  }}
+	title="Stockify"
+	titleTemplate="%s | Learn how to invest"
+	description="Learn how to trade and invest with stockify's real life simulator, at no risk."
+	openGraph={{
+		url: 'https://stockify.prathamprasoon.com',
+		title: 'Stockify',
+		description: "Learn how to trade and invest with stockify's real life simulator, at no risk.",
+		images: [
+			{
+				url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png',
+				width: 800,
+				height: 600,
+				alt: 'Stockify cover image'
+			},
+			{
+				url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png',
+				width: 900,
+				height: 800,
+				alt: 'Stockify cover image'
+			},
+			{
+				url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png'
+			},
+			{
+				url: 'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png'
+			}
+		],
+		site_name: 'Stockify'
+	}}
+	twitter={{
+		handle: '@Stockify_',
+		cardType: 'summary_large_image',
+		title: 'Stockify | Learn how to invest',
+		description: "Learn how to trade and invest with stockify's real life simulator, at no risk.",
+		image:
+			'https://raw.githubusercontent.com/PrasoonPratham/Stock-app-frontend/main/static/cover.png',
+		imageAlt: 'Stockify cover image'
+	}}
 />
 
 <Nav />
