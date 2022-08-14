7bcbc9fb45b98b490fb80ab5a0bbd776379ed526
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-08-14 20:18:09 +0530

    Changes to navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,13 @@
 <script>
 	import '../app.css';
-	import Nav from '../components/nav.svelte'
+	import Nav from '../components/nav.svelte';
+	import { onMount } from 'svelte'
+  	import { themeChange } from 'theme-change'
+
+	onMount(() => {
+    	themeChange(false)
+  	})
 </script>
 
-<Nav/>
+<Nav />
 <slot />
