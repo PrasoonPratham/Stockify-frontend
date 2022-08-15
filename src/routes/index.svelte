816b33470d91b54be1b168a86442c6adf2508b0a
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-08-15 21:44:47 +0530

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="hero min-h-screen bg-gradient-to-t from-base-300 to-base-100">
+<div class="hero min-h-screen bg-gradient-to-b from-base-100 to-base-300">
 	<div class="hero-content text-center">
 		<div class="max-w-lg">
 			<p class="text-2xl md:text-4xl lg:text-5xl font-medium font-poppins">Work In Progress 👀🛠</p>
